75bc9082160d77e5c353d8d480781fca75a74b5d
shida-zheng-ICD2O1-01 <shida.zheng@student.tdsb.on.ca>
2024-10-16 16:51:42 +0000

    a

--- PP2_3.py
+++ PP2_3.py
@@ -2,12 +2,24 @@
 
 def q1(): 
   #Write Assignment code here
-
-
+  a = input("Input a word:")
+  if a.endswith("y"):
+    print("-ies")
+  elif a.endswith("ey"):
+    print("-eys")
+  elif a.endswith("ife"):
+    print("-ives")
+  else:
+    print("-s")
 def q2(): 
   #Write Assignment code here
-
-
+  num = input("Input a integer:")
+  a = int(num)
+  if a > 0:
+    print(f"{a} is positive")
+  else:
+    print(f"{a} is negative")
+    
 
 #Do not alter the following code
 #Comment out the following code when running your tests
